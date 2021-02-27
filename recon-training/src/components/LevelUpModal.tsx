3bf0ebb3c42c051dc2b7a9bf3e26a1bc1b6d8bd0
Sylvn001 <juniorbaarros@gmail.com>
2021-02-27 16:07:26 -0300

    Dark theme to next level app

--- recon-training/src/components/LevelUpModal.tsx
+++ recon-training/src/components/LevelUpModal.tsx
@@ -16,6 +16,17 @@ export function LevelUpModal(){
         <button type="button" onClick={closeLevelUpModal}>
           <img src="/icons/close.svg" alt="fechar modal"/>
         </button>
+        <div className={styles.containerTwitter}>
+          <a 
+            className="twitter-share-button"
+            href={`https://twitter.com/intent/tweet?text=Alcancei%20ao%20level%20${level}!!%20Recon-Training%20https://recon-training-b50rn51mh-sylvn001.vercel.app/`}
+            target="_blank"
+            data-size="large"
+          >
+            <strong>Compartilhar no Twitter!</strong>
+            <img src="icons/twitter.svg"/>
+          </a>
+        </div>
       </div>
     </div>
   )
